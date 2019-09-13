20bd548dd9555d1c0de4b16fbef261eb2f1eece6
unknown <jjamal95@gmail.com>
2019-09-12 20:50:53 -0500

    finished Auth testing

--- api/server.js
+++ api/server.js
@@ -13,12 +13,27 @@ server.use('/api/events' , authenticate , eventsRouter)
 server.use('/api/vendors', authenticate, vendorsRouter)
 
 
-server.get("/", (req, res) => {
-    res.send(`
-    <h2> Corporate Events Planner </h2>
-    <p> Welcome to the Events Database API </p>
+// server.get("/", (req, res) => {
+//     res.send(`
+//     <h2> Corporate Events Planner </h2>
+//     <p> Welcome to the Events Database API </p>
     
-    `);
+//     `);
+// });
+
+
+//TESTING
+
+server.get("/", (req, res) => {
+    res.status(200).json({api:'running'});
+});
+
+server.post("/register", (req, res) => {
+    res.status(201).json({status: "CREATED"});
+});
+
+server.post('/login', (req, res) => {
+    res.status(200).json({status: 'OK'});
 });
 
 module.exports = server;
