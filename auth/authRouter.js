eb64602466b14efc3be6f1f72d453db0cbbda014
unknown <jjamal95@gmail.com>
2019-09-14 00:43:08 -0500

    working on merging

---
+++ auth/authRouter.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const userDb = require('./authModel')
+const authenticate = require('../auth/authenticate-middleware')
+//const restricted = require('../auth/authenticate-middleware');
+require('dotenv').config();
+const router = express.Router();
+
+router.post('/register', (req, res) => {
+    const user = req.body;
+    const hash = bcrypt.hashSync(user.password, 12);
+    user.password = hash;
+
+    userDb.create(user)
+    .then(u => {
+        res.status(201).json({message: `Welcome ${user.username}`})
+    })
+    .catch(err => {
+      console.log(err);
+        res.status(500).json({error: err})
+    })
+
+})
+
+router.post('/login', (req, res) => {
+    let {username, password} = req.body;
+    userDb.findBy({username})
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const tokenHeader = generateToken(user);
+        res.status(200).json({message: `Hi ${user.username}`, tokenHeader})
+      } else {
+        res.status(401).json({message: 'Easy there, you cannot pass'})
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+  });
+  
+  function generateToken(user) {
+    const payload = {
+      subject: user.id,
+      username: user.username
+    }
+    const secret = process.env.SECRET || "$$$$$$$"
+  const options = {
+    expiresIn: '1h'
+  }
+  return jwt.sign(payload, secret, options);
+  };
+
+
+router.get("/events",  authenticate, (req, res) => {
+    userDb.fetch()
+    .then(events => {
+      res.json(events);
+    }) .catch( error => res.send(error));
+});
+
+
+module.exports = router;
