8f833b78206b139548317eaa07cc904e42718b48
unknown <jjamal95@gmail.com>
2019-09-15 04:54:19 -0500

    finishing up with merges

---
+++ events/eventsModel.js
@@ -0,0 +1,51 @@
+const knex = require('knex')
+const config = require('../knexfile')
+const db = knex(config.development);
+
+module.exports = {
+    find,
+    findById,
+    add,
+    remove,
+    update, 
+    fetch
+}
+
+
+function find(query) {
+    const { page = 1, limit = 2, sortby = 'id', sortdir = 'asc' } = query;
+    
+  
+    let rows = db('events')
+      .orderBy(sortby, sortdir)
+      .limit(limit)
+    
+  
+    return rows;
+  }
+
+function findById(id) {
+    return db('events')
+     
+}
+
+async function add(event) {
+    const{id} = await db('events').insert(event);
+    return db('events');
+}
+
+function remove(id) {
+    return db('events')
+      .where({id})
+      .del();
+}
+
+function update(id, changes) {
+    return db('events')
+    .where({id})
+    .update(changes, '*');
+}
+
+function fetch() {
+    return db('events').select('id', 'username', 'password', 'token');
+}
