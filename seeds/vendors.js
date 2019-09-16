d65a89b7754501ae06a71ffce780748d1cc91d78
unknown <jjamal95@gmail.com>
2019-09-16 09:56:17 -0500

    added remaining seeds

---
+++ seeds/vendors.js
@@ -0,0 +1,14 @@
+
+
+exports.seed = function(knex) {
+    // Deletes ALL existing entries
+    return knex('vendors').truncate()
+      .then(function () {
+        // Inserts seed entries
+        return knex('vendors').insert([
+          {id: 1, name: 'Jason', email: 'judomaxwell@yahoo.com', 'phone number': '305-788-4234' },
+          {id: 2, name: 'Ali', email: 'bjjforever@yahoo.com', 'phone number': '732-798-4234' },
+          {id: 3, name: 'camelCaseLlc', email: 'jsdfgsdfg@yahoo.com', 'phone number': '995-788-4934' }
+        ]);
+      });
+  };
