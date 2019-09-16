914ae52047c19d601b694da15e3f2924c3e30e26
unknown <jjamal95@gmail.com>
2019-09-16 11:12:16 -0500

    made config to .env and index.js for deployment

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-
+require('dotenv').config();
 
 
 const server = require('./api/server');
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 server.listen(port, () => {
     console.log(`server listening on port ${port}...`);
